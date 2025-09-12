9750d05f13dcc125080b2acdf5221e6fb5a59bb5
Maxim Menshikov <maxim.menshikov@interpretica.io>
2025-09-12 01:43:35 +0100

    ta_wifi: improve standard documentation
    Signed-off-by: Maxim Menshikov <maxim.menshikov@interpretica.io>

--- lib/ta_wifi/ta_wifi_internal.h
+++ lib/ta_wifi/ta_wifi_internal.h
@@ -38,8 +38,8 @@ typedef enum ta_wifi_standard {
     TA_WIFI_STANDARD_G = 0,             /**< G standard (2.4GHz) */
     TA_WIFI_STANDARD_N,                 /**< N standard (2.4GHz) */
     TA_WIFI_STANDARD_AC,                /**< AC standard (5GHz) */
-    TA_WIFI_STANDARD_AX,                /**< AX standard (5GHz) */
-    TA_WIFI_STANDARD_BE,                /**< BE standard (6GHz) */
+    TA_WIFI_STANDARD_AX,                /**< AX standard (2.4/5GHz) */
+    TA_WIFI_STANDARD_BE,                /**< BE standard (2.4/5/6GHz) */
 } ta_wifi_standard;
 
 /** Supported WiFi bandwidths */
